3580ddb3ca0984e3bc25ec5aef13d925d0499be4
Merksue <gabeaboy@gmail.com>
2016-12-02 14:17:15 -0500

    list in order, search works, click into selected box

--- README.md
+++ README.md
@@ -9,9 +9,17 @@
 
 #December 1
 *complete
-1/2/*
+1/2/4*
 1. Battle select Skeleton
 2. Alphabetical order
 3. Search
 4. css center alignment
 5. Fill in the blank boxes / names
+
+#December 2
+3. DONE
+5. DONE
+*Comments on 3 and 5*
+3 should also reference type or have buttons for that like lol does
+5 should not be able to override click over playerTwo
+  and should start a timer
--- controller.js
+++ controller.js
@@ -9,11 +9,21 @@ angular.module('lolApp').controller('mainCtrl', function($scope, mainService) {
   };
   $scope.getUsers();
   // $scope.name= champ;
-
+  $scope.lockedIn = false;
   $scope.selectChamp = function(champ) {
-    console.log(champ);
-
+    if($scope.lockedIn){
+      $scope.playerTwo = champ;//;
+    }
+    else $scope.playerOne = champ;//.image.full;
   };
+  $scope.lockIn = function() {
+  if($scope.playerOne) $scope.lockedIn = true;
+  //if(player two defined move on to countdown)
+  };
+  setTimeout(function () {
+    console.log($scope.playerTwo);
+    console.log($scope.playerOne);
+  }, 2000);
 
 
 });
